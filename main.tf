dcf06ca4158fa69dae544cddc14507767cd6252f
João <jancunha@gmail.com>
2022-08-06 16:25:19 -0300

    2Gb para 4Gb

--- main.tf
+++ main.tf
@@ -10,3 +10,15 @@ terraform {
 provider "digitalocean" {
   token = "dop_v1_bd2da5e99e2ea65457d6c1eefbbb112b7603af7636d888009e5ef9c4cad89bfb"
 }
+
+resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
+  name   = "k8s-iniciativa"
+  region = "nyc1"
+  version = "1.23.9-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-4gb"
+    node_count = 3
+  }
+}
